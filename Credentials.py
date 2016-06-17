3ebf39d4c780e3bcfedee5fc9c2d3b326c588438
Priyaa140624191 <P.Thavasimani2@newcastle.ac.uk>
2016-06-17 16:31:28 +0100

    Twitter Credentials

---
+++ Credentials.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generated via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+consumer_key="SECRET"
+consumer_secret="SECRET"
+access_token="SECRET"
+access_token_secret="SECRET"
